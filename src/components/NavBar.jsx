f720db040ec1c7386037396919c381c9777587c6
Pipexlul <felipgg@gmail.com>
2023-03-18 09:51:10 -0300

    Add first part of NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,58 @@
+import { Link, NavLink } from "react-router-dom";
+
+import ShinyNavLink from "./ShinyNavLink";
+
+const NavBar = ({
+  header,
+  headerExtra,
+  headerGradient,
+  navLinks,
+  bgLinkColor,
+  textLinkColor,
+}) => {
+  return (
+    <header className="bg-gray-900">
+      <div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
+        <h1>
+          <Link className="text-2xl font-extrabold" to="/home">
+            <span className={`bg-clip-text text-transparent ${headerGradient}`}>
+              {header}
+            </span>
+            {headerExtra && (
+              <span className="bg-clip-border text-white text-2xl font-extrabold">
+                {headerExtra}
+              </span>
+            )}
+          </Link>
+        </h1>
+
+        <div className="flex flex-1 items-center justify-between">
+          <nav>
+            <ul className="flex items-center gap-6 text-sm">
+              {navLinks.map(({ label, path }, idx) => {
+                return (
+                  <li key={idx}>
+                    <NavLink to={path}>
+                      {({ isActive }) => {
+                        return (
+                          <ShinyNavLink
+                            label={label}
+                            bgColor={bgLinkColor}
+                            labelColor={textLinkColor}
+                            isActive={isActive}
+                          />
+                        );
+                      }}
+                    </NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default NavBar;
--- src/views/Root.jsx
+++ src/views/Root.jsx
@@ -1,9 +1,20 @@
 import { Outlet } from "react-router-dom";
 
+import NavBar from "../components/NavBar";
+
+import { navData } from "../data/navData.json";
+
 const Root = () => {
   return (
     <div>
-      <h1>Root</h1>
+      <NavBar
+        header="Mamma Mia Pizzeria "
+        headerExtra="🍕 👩‍🍳"
+        headerGradient="bg-gradient-to-r from-red-600 via-white to-green-500"
+        bgLinkColor="bg-green-500"
+        textLinkColor="text-white"
+        navLinks={navData}
+      />
       <Outlet />
     </div>
   );
